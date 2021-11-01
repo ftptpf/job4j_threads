d72cca8a7d9b13c9c140347d27476ec55e4d952f
ftptpf <inboxru@yandex.ru>
2021-11-01 11:39:06 +0300

    1.2. Режим ожидания. [#231217] - Wget

---
+++ src/main/java/ru/job4j/multithreading/threads/Wget.java
@@ -0,0 +1,24 @@
+package ru.job4j.multithreading.threads;
+
+/**
+ * Имитация счетчика загрузки от 0 % до 100 %.
+ * Каждую секунду на консоли обновляется информация.
+ * Вывод с обновлением в одной и тоже же строке.
+ */
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    for (int i = 0; i <= 100; i++) {
+                        try {
+                            System.out.print("\rLoading: " + i + "%");
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+    }
+}
