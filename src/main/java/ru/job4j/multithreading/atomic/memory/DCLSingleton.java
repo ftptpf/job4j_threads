bc52abcf8bdb202ed4096489517f955e5e877b5f
ftptpf <Nb49Rtdd>
2021-11-07 17:40:29 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/multithreading/atomic/memory/DCLSingleton.java
@@ -0,0 +1,18 @@
+package ru.job4j.multithreading.atomic.memory;
+
+public class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+    private DCLSingleton() {
+    }
+}
