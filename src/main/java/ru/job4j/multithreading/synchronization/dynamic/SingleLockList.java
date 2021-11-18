9b5f75ba7d1f8895b2b1ebb26f368ac8067958c2
ftptpf <Nb49Rtdd>
2021-11-18 21:52:42 +0300

    4. ThreadSafe динамический список [#1105] - clone

--- src/main/java/ru/job4j/multithreading/synchronization/dynamic/SingleLockList.java
+++ src/main/java/ru/job4j/multithreading/synchronization/dynamic/SingleLockList.java
@@ -1,31 +1,48 @@
 package ru.job4j.multithreading.synchronization.dynamic;
 
+import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.Iterator;
 import java.util.List;
 
-/*@ThreadSafe
+/**
+ * Коллекция которая будет корректно работать в многопоточной среде.
+ * @param <T>
+ */
+
+@ThreadSafe
 public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
     private final List<T> list;
 
     public SingleLockList() {
     }
 
     public SingleLockList(List<T> list) {
-        this.list = (List) list.clone();
-    }
-
-    public synchronized void add(T value) {
+        this.list = list.clone();
 
     }
 
-    public synchronized T get(int index) {
-        return null;
+    public void add(T value) {
+        synchronized (this) {
+            list.add(value);
+        }
+    }
+
+    public T get(int index) {
+        synchronized (this) {
+            return list.get(index);
+        }
     }
 
     @Override
     public synchronized Iterator<T> iterator() {
         return copy(this.array).iterator();
     }
-}*/
+
+    @Override
+    public List<T> clone() throws CloneNotSupportedException {
+        return (List<T>) super.clone();
+    }
+}
