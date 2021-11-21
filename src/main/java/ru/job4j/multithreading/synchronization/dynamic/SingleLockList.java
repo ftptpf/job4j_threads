23e2710294cbd00d8c5ebdb0e73724bc69c3f408
ftptpf <Nb49Rtdd>
2021-11-21 19:15:03 +0300

    4. ThreadSafe динамический список [#1105] - конструктор

--- src/main/java/ru/job4j/multithreading/synchronization/dynamic/SingleLockList.java
+++ src/main/java/ru/job4j/multithreading/synchronization/dynamic/SingleLockList.java
@@ -3,11 +3,12 @@ package ru.job4j.multithreading.synchronization.dynamic;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 /**
- * Коллекция которая будет корректно работать в многопоточной среде.
+ * Коллекция которая корректно работает в многопоточной среде.
  * @param <T>
  */
 
@@ -17,11 +18,11 @@ public class SingleLockList<T> implements Iterable<T> {
     private final List<T> list;
 
     public SingleLockList() {
+        list = new ArrayList<>();
     }
 
     public SingleLockList(List<T> list) {
-        this.list = list.clone();
-
+        this.list = new SingleLockList<T>(list).cloneList();
     }
 
     public void add(T value) {
@@ -38,11 +39,17 @@ public class SingleLockList<T> implements Iterable<T> {
 
     @Override
     public synchronized Iterator<T> iterator() {
-        return copy(this.array).iterator();
+        return cloneList().iterator();
+
     }
 
-    @Override
-    public List<T> clone() throws CloneNotSupportedException {
-        return (List<T>) super.clone();
+    public List<T> cloneList() {
+        List<T> listClone = new ArrayList<>();
+        synchronized (this) {
+            for (int i = 0; i < list.size(); i++) {
+                listClone.add(i, list.get(i));
+            }
+        }
+        return listClone;
     }
 }
