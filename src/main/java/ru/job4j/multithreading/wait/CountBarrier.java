0ec6eccdd31cf1a91da04b35d097fe69b37344c9
ftptpf <inboxru@yandex.ru>
2021-11-23 09:53:39 +0300

    0. Управление нитью через wait. [#6858] - monitor

--- src/main/java/ru/job4j/multithreading/wait/CountBarrier.java
+++ src/main/java/ru/job4j/multithreading/wait/CountBarrier.java
@@ -1,12 +1,17 @@
 package ru.job4j.multithreading.wait;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 /**
  * Класс блокирует выполнение по условию счетчика.
  * Переменная total содержит количество вызовов метода count().
  * Метод count изменяет состояние программы.
  *
  */
+@ThreadSafe
 public class CountBarrier {
+    @GuardedBy("monitor")
     private final Object monitor = this;
     private final int total;
     private int count = 0;
@@ -16,10 +21,60 @@ public class CountBarrier {
     }
 
     public void count() {
-
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
     }
 
     public void await() {
+        synchronized (monitor) {
+            System.out.println("Count is " + count);
+            while (count >= total) {
+                System.out.println("Count is " + count + " " + Thread.currentThread().getName() + "Do the job");
+                count = 0;
+            }
+            try {
+                monitor.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CountBarrier countBarrier = new CountBarrier(4);
+/*        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    countBarrier.await();
+                }, "First"
+        );
+        Thread second = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    countBarrier.await();
+                }, "Second"
+        );
+        Thread third = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    countBarrier.await();
+                }, "Third"
+        );*/
+
+/*        first.start();
+        second.start();
+        third.start();*/
+        for (int i = 0; i < 5; i++) {
+            countBarrier.count();
+            new Thread(
+                    () -> {
+                        System.out.println(Thread.currentThread().getName() + " started");
+                        countBarrier.await();
+                    }
+            );
+        }
 
     }
 }
