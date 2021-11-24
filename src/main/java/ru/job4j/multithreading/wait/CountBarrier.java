fa5e359328e6ea8eba7ddf93d74fe6ef1f85e47d
ftptpf <inboxru@yandex.ru>
2021-11-24 09:01:59 +0300

    0. Управление нитью через wait. [#6858] - counterThread, jobThread

--- src/main/java/ru/job4j/multithreading/wait/CountBarrier.java
+++ src/main/java/ru/job4j/multithreading/wait/CountBarrier.java
@@ -5,9 +5,11 @@ import net.jcip.annotations.ThreadSafe;
 
 /**
  * Класс блокирует выполнение по условию счетчика.
- * Переменная total содержит количество вызовов метода count().
- * Метод count изменяет состояние программы.
- *
+ * counterThread - увеличивает счетчик
+ * jobThread - выполняет работу
+ * Пока параметр счетчика будет (count < total),
+ * jobThread будет находиться в состоянии ожидания (wait),
+ * послед выполнения условия - jobThread выполнит свою работу (выведет сообщение не экран).
  */
 @ThreadSafe
 public class CountBarrier {
@@ -23,58 +25,43 @@ public class CountBarrier {
     public void count() {
         synchronized (monitor) {
             count++;
+            System.out.println("Count is " + count + ",  " + Thread.currentThread().getName() + " - increase counter");
             monitor.notifyAll();
         }
     }
 
     public void await() {
         synchronized (monitor) {
-            System.out.println("Count is " + count);
-            while (count >= total) {
-                System.out.println("Count is " + count + " " + Thread.currentThread().getName() + "Do the job");
-                count = 0;
+            while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
             }
-            try {
-                monitor.wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
+            System.out.println("Count is " + count + " " + Thread.currentThread().getName() + " Do job after wait");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         CountBarrier countBarrier = new CountBarrier(4);
-/*        Thread first = new Thread(
+        Thread counterThread = new Thread(
                 () -> {
-                    System.out.println(Thread.currentThread().getName() + " started");
-                    countBarrier.await();
-                }, "First"
-        );
-        Thread second = new Thread(
-                () -> {
-                    System.out.println(Thread.currentThread().getName() + " started");
-                    countBarrier.await();
-                }, "Second"
-        );
-        Thread third = new Thread(
-                () -> {
-                    System.out.println(Thread.currentThread().getName() + " started");
-                    countBarrier.await();
-                }, "Third"
-        );*/
-
-/*        first.start();
-        second.start();
-        third.start();*/
-        for (int i = 0; i < 5; i++) {
-            countBarrier.count();
-            new Thread(
-                    () -> {
-                        System.out.println(Thread.currentThread().getName() + " started");
-                        countBarrier.await();
+                    System.out.println(Thread.currentThread().getName() + " Started");
+                    for (int i = 0; i < 50; i++) {
+                        countBarrier.count();
                     }
-            );
-        }
+                }, "Counter Thread"
+        );
+        Thread jobThread
+                = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName() + " Started");
+                }, "Job Thread"
+        );
 
+        jobThread.start();
+        counterThread.start();
     }
 }
