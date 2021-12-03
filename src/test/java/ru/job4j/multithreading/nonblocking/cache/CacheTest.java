9035b66bc0c9b0d1f4b763cb8dc6b5e9425686da
ftptpf <Nb49Rtdd>
2021-12-03 09:13:24 +0300

    1. Неблокирующий кеш [#4741] - tru-catch in test

--- src/test/java/ru/job4j/multithreading/nonblocking/cache/CacheTest.java
+++ src/test/java/ru/job4j/multithreading/nonblocking/cache/CacheTest.java
@@ -1,15 +1,12 @@
 package ru.job4j.multithreading.nonblocking.cache;
 
-import org.junit.Rule;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
 public class CacheTest {
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void test() throws InterruptedException {
@@ -22,7 +19,11 @@ public class CacheTest {
                 () -> {
                     Base baseOne = new Base(1, 1);
                     baseOne.setName("Base #1");
-                    cache.update(baseOne);
+                    try {
+                        cache.update(baseOne);
+                    } catch (OptimisticException oe) {
+                        Assert.assertNotEquals("", oe.getMessage());
+                    }
                 }, "First"
         );
         Thread second = new Thread(
@@ -34,16 +35,17 @@ public class CacheTest {
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                     }
-                    cache.update(baseTwo);
+                    try {
+                        cache.update(baseTwo);
+                    } catch (OptimisticException oe) {
+                        Assert.assertNotEquals("", oe.getMessage());
+                    }
                 }, "Second"
         );
         first.start();
         second.start();
         first.join();
         second.join();
-        expectedException.expect(OptimisticException.class);
-        //expectedException.expectMessage("version has already changed");
-        expectedException.expectMessage(is("version has already changed"));
         assertThat(cache.toString(), is("Cache{memory={1=Base{id=1, version=2, name='Base #1'}}}"));
     }
 }
