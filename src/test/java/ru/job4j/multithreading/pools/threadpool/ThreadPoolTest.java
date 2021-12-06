4d77a429207581dc698bf48d5d8a79eca0f0679e
ftptpf <Nb49Rtdd>
2021-12-06 22:13:35 +0300

    1. Реализовать ThreadPool [#1099] - Thread

--- src/test/java/ru/job4j/multithreading/pools/threadpool/ThreadPoolTest.java
+++ src/test/java/ru/job4j/multithreading/pools/threadpool/ThreadPoolTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.multithreading.pools.threadpool;
 
 import org.junit.Test;
+import ru.job4j.multithreading.wait.producer.SimpleBlockingQueue;
 
 import java.util.stream.IntStream;
 
@@ -13,17 +14,37 @@ public class ThreadPoolTest {
     @Test
     public void test() throws InterruptedException {
         ThreadPool threadPool = new ThreadPool();
-        Generator generator = new Generator();
-        threadPool.work(generator);
+        Producer producer = new Producer();
+        threadPool.work(producer);
+        SimpleBlockingQueue<Runnable> queue = new SimpleBlockingQueue<>(5);
+
+        Thread thread = new Thread(
+                () -> {
+                    for (int i = 0; i < 20; i++) {
+                        try {
+                            queue.offer(new Producer());
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        thread.start();
+
+
+
+
 
     }
 
-    private class Generator implements Runnable {
+    private class Producer implements Runnable {
 
         @Override
         public void run() {
             int size = Runtime.getRuntime().availableProcessors();
-            System.out.println(IntStream.range(0, size));
+            System.out.print(Thread.currentThread().getName() + " " + IntStream.range(0, size));
+            System.out.println();
         }
     }
 }
