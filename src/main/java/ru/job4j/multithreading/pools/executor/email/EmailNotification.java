84f4b77f7cb54abb11c0bd308617c3f3cd536e0d
ftptpf <Nb49Rtdd>
2021-12-09 22:48:27 +0300

    2. ExecutorService рассылка почты. [#63097] - execute

--- src/main/java/ru/job4j/multithreading/pools/executor/email/EmailNotification.java
+++ src/main/java/ru/job4j/multithreading/pools/executor/email/EmailNotification.java
@@ -3,9 +3,10 @@ package ru.job4j.multithreading.pools.executor.email;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Рассылка почты.
@@ -20,31 +21,44 @@ public class EmailNotification {
      * Осуществляем подготовку данных для отправки.
      * @param user
      */
-    public void emailTo(User user) {
-        String subject = "Notification {username} to email {email}";
-        String body = "Add a new event to {username}";
+    public synchronized void emailTo(User user) {
         String username = user.getUsername();
         String email = user.getEmail();
-        pool.submit(() -> {
-
+        String subject = "Notification " + username + " to email " + email;
+        String body = "Add a new event to " + username;
+        pool.execute(() -> {
+            send(subject, body, email);
         });
-
     }
 
     /**
      * Закрываем пул потоков.
      */
-    public void close() throws InterruptedException {
+    public synchronized void close() {
         pool.shutdown();
-        pool.awaitTermination(10L, TimeUnit.MINUTES);
     }
 
     /**
-     * Отправляем сообщение. Метод оставлен пустым.
+     * Отправляем сообщение. Метод оставлен пустым, только вывод на консоль для демонстрации.
      * @param subject тема письма
      * @param body содержание письма
      * @param email электронный адрес
      */
-    public void send(String subject, String body, String email) {
+    public synchronized void send(String subject, String body, String email) {
+        System.out.println("Subject: " + subject + " Body: " + body + " Email: " + email);
+    }
+
+    public static void main(String[] args) {
+        EmailNotification emailNotification = new EmailNotification();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Andy", "andy@mail.ru"));
+        users.add(new User("Tony", "tony@mail.ru"));
+        users.add(new User("Liza", "liza@mail.ru"));
+        users.add(new User("Igor", "igor@mail.ru"));
+        users.add(new User("Oleg", "oleg@mail.ru"));
+        for (User user : users) {
+            emailNotification.emailTo(user);
+        }
+        emailNotification.close();
     }
 }
