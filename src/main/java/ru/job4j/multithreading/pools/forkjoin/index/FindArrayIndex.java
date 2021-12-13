0476c30be770ae3e62a277becf3e1bd3bf3bf664
ftptpf <Nb49Rtdd>
2021-12-13 22:12:23 +0300

    3. ForkJoinPool [#315067] - main

--- src/main/java/ru/job4j/multithreading/pools/forkjoin/index/FindArrayIndex.java
+++ src/main/java/ru/job4j/multithreading/pools/forkjoin/index/FindArrayIndex.java
@@ -11,6 +11,7 @@ import java.util.concurrent.RecursiveTask;
  * Если размер массива не больше 10, используем обычный линейный поиск,
  * в ином случае параллельный поиск.
  * Метод поиска обобщенный.
+ * Если объекта нет в массиве - выводим отрицательно значение индекса.
  */
 @ThreadSafe
 public class FindArrayIndex<V> extends RecursiveTask<Integer> {
@@ -28,6 +29,11 @@ public class FindArrayIndex<V> extends RecursiveTask<Integer> {
         int size = array.length;
         if (size <= 10) {
             for (int i = 0; i < size; i++) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
                 System.out.println("Value: " + array[i] + " index: " + i);
                 if (array[i].equals(value)) {
                     return i;
@@ -36,27 +42,25 @@ public class FindArrayIndex<V> extends RecursiveTask<Integer> {
         } else {
             int mid = size / 2;
             FindArrayIndex<V> left = new FindArrayIndex<V>(Arrays.copyOfRange(array, 0, mid), value);
-            FindArrayIndex<V> right = new FindArrayIndex<V>(Arrays.copyOfRange(array, mid + 1, size - 1), value);
-            left.fork();
-            right.fork();
-            int l = left.join();
-            int r = right.join();
-            System.out.println("size: " + size + " mid: " + mid + " left: " + l + " right: " + r + " Max: " + Math.max(l, r));
+            FindArrayIndex<V> right = new FindArrayIndex<V>(Arrays.copyOfRange(array, mid, size), value);
+            int l = left.invoke();
+            int r = right.invoke();
+            System.out.println("size: " + size + " mid: " + mid + " left: " + l + " right: " + r);
             if (l >= 0) {
                 return l;
             }
             if (r >= 0) {
-                return r + l;
+                return r + mid;
             }
-            return -1;
+            return l + r;
         }
         return -1;
     }
 
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
-        Integer[] array = {2, 3, 5, 6, 8, 99, 23, 21, 45, 121, 12, 12, 56, 32, 11, 4, 58, 23, 90, 6, 77, 88, 21};
-        int result = forkJoinPool.invoke(new FindArrayIndex<>(array, 77));
+        Integer[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+        int result = forkJoinPool.invoke(new FindArrayIndex<>(array, 22));
         System.out.println(result);
     }
 }
